b6fcd45763891e0bffb54ed5b1b478a242e19215
JavaEEPRO <j2eengineer@gmail.com>
2019-11-07 14:25:57 +0200

    UserController refactoring : methods, that works with messages, are moved out, into the MessageController.class

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ simple chat - test task "simple web app"
   
     UserController:
     
-    \join\   -  adds new user with default user name, returns json with user entity
+    /join/   -  adds new user with default user name, and returns json with user' fields
      
-    \join\{name} -  accepts new user with given name, returns json with user entity, where name is set to given 
+    /join/{name} -  accepts new user with given name, and returns json with user' fields, where name=name
+    
+    /messages - returns common sorted list with all messages, that were posted 
+    
+    /messages/{name}/say/{content} - if user with name=name is present, message with content=content will be posted 
---
+++ src/main/java/si/inspirited/web/controller/MessageController.java
@@ -0,0 +1,45 @@
+package si.inspirited.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.RedirectView;
+import si.inspirited.persistence.model.Message;
+import si.inspirited.service.IMessageService;
+import si.inspirited.service.IUserService;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class MessageController {
+
+    @Autowired
+    IUserService userService;
+
+    @Autowired
+    IMessageService messageService;
+
+    @RequestMapping(value = { "/messages", "/messages/{name}" }, method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public List<Message> refreshSortedMessagesList(@PathVariable
+                                                           Optional<String> name) {
+        return messageService.getAllSortedMessages();
+    }
+
+    @RequestMapping(value = { "messages/{name}/say", "/messages/{name}/say/{content}" }, method = RequestMethod.GET, produces = { MediaType.APPLICATION_JSON_VALUE })
+    @ResponseBody
+    public RedirectView postMessage(@PathVariable
+                                            String name,
+
+                                    @PathVariable
+                                            String content) {
+        messageService.addNewMessage(content, name);
+
+        return new RedirectView("/messages");
+    }
+}
